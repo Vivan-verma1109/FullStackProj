64310a67e91e6b33c526d4b28785a40056c7b608
Vivan-verma1109 <vivanv732@gmail.com>
2024-06-30 12:22:26 -0700

    updating server

--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const planetsRouter = express.Router();
 
+const{
+    getAllPlanets,
+
+} = require("./planets.controller")
+
 planetsRouter.get('/planets', getAllPlanets)
 
 
